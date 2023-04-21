20d612cd651cb377eb18984c82801c1d2ce0f8c4
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-21 23:17:48 +0530

    Create Simple Statistics.java

---
+++ Simple Statistics.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Arrays.sort(a);
+            double sum = 0;
+            for (int i = k; i < n - k; i++) {
+                sum += a[i];
+            }
+            System.out.println(sum / (n - 2 * k));
+        }
+		// your code goes here
+	}
+}

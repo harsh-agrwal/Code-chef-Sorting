93af71b6acc172c1aade4276f49cce37d9fc1ba9
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-22 23:56:02 +0530

    Create Magic Pairs.java

---
+++ Magic Pairs.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Arrays.sort(a);
+            long count = 0;
+            for (int i = 0; i < n - 1; i++) {
+                count += n - i - 1;
+            }
+            System.out.println(count);
+        }
+        sc.close();
+		// your code goes here
+	}
+}

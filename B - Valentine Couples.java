0eb9db3c274b1d3ea80145526b48c9449ac38d79
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-13 19:24:22 +0530

    Create B - Valentine Couples.java

---
+++ B - Valentine Couples.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0 ){
+		    int n = sc.nextInt();
+		    int a[] = new int[n];
+		    for (int i = 0 ; i < n ; i++ ) a[i] = sc.nextInt();
+		    int b[] = new int[n];
+		    for (int i = 0 ; i < n ; i++ ) b[i] = sc.nextInt();
+		    Arrays.sort( a );
+		    Arrays.sort( b );
+		    long ans = 0;
+		    for( int i = 0 ; i < n ; i++ ){
+		        ans = Math.max( a[i] + b[ n - i - 1 ] , ans );
+		    }
+		    System.out.println( ans );
+		}
+	}
+}
